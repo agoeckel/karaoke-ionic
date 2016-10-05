8a62776310b442a9adced3c946feb184a7a5cbb3
Alec <email": null>
2016-10-04 18:00:27 -0700

    Add spotify method

--- www/app/playlist/playlists-ctrl.js
+++ www/app/playlist/playlists-ctrl.js
@@ -1,29 +1,41 @@
+
 karaoke.controller('PlaylistsCtrl', ['$scope', '$http', function($scope, $http) {
 
+function setHeader() {
+  return {
+    "access-token": window.sessionStorage.token,
+    "token-type": "Bearer",
+    "client": window.sessionStorage.client,
+    "expiry": window.sessionStorage.expiry,
+    "uid": window.sessionStorage.uid,
+    "id": window.sessionStorage.id
+  };
+};
+
+  console.log($scope.query)
+
   $scope.playlist = function(){
-    console.log("hello")
-    $http.get(rootUrl + "/api/playlists/1")
+    $http.get(rootUrl + "/api/playlists", {headers: setHeader()})
       .success(function(response){
         $scope.playlists = response.songs;
-    }).fail(function(){
-      showAlert("fail")
     })
   }
 
-  // $http.get(rootUrl + $scope.playlists.id + '/playlists' + $scope.playlists.id)
-  //   .success(function(response){
-  //     $scope.userplaylist = response
-  //   })
+  $scope.spotify = function() {
+    $http.get(rootUrl + '/api/artists', {
+      headers: setHeader(),
+      params: {track_name: "Sublime"}
+    })
+      .success(function(response){
+        $scope.songs = response
+        console.log(response)
+      })
+  }
 
-    $scope.playlistParty
-    // $scope.user
+  $scope.songInfo = function(){
 
 
-    // $scope.createParty = function(){
 
-    //   $http.post(
-    //       rootUrl + "/playlists",
-    //       {title: $scope.playlistCreate}
-    //     )
-    // }
+  }
+
 }]);
