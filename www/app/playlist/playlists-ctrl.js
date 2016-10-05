00b4c3f653ec49b4702ce303869ec097f473b15f
Alec <email": null>
2016-10-04 19:45:23 -0700

    Add load before enter

--- www/app/playlist/playlists-ctrl.js
+++ www/app/playlist/playlists-ctrl.js
@@ -1,30 +1,31 @@
 
 karaoke.controller('PlaylistsCtrl', ['$scope', '$http', function($scope, $http) {
 
-function setHeader() {
-  return {
-    "access-token": window.sessionStorage.token,
-    "token-type": "Bearer",
-    "client": window.sessionStorage.client,
-    "expiry": window.sessionStorage.expiry,
-    "uid": window.sessionStorage.uid,
-    "id": window.sessionStorage.id
-  };
-};
 
-  console.log($scope.query)
+  function setHeader() {
+    return {
+      "access-token": window.sessionStorage.token,
+      "token-type": "Bearer",
+      "client": window.sessionStorage.client,
+      "expiry": window.sessionStorage.expiry,
+      "uid": window.sessionStorage.uid,
+      "id": window.sessionStorage.id
+    };
+  };
 
   $scope.playlist = function(){
     $http.get(rootUrl + "/api/playlists", {headers: setHeader()})
       .success(function(response){
+        console.log(response)
         $scope.playlists = response.songs;
     })
   }
 
   $scope.spotify = function() {
+    song = $scope.spotify.searchedSong
     $http.get(rootUrl + '/api/artists', {
       headers: setHeader(),
-      params: {track_name: "Sublime"}
+      params: {track_name: song}
     })
       .success(function(response){
         $scope.songs = response
@@ -33,9 +34,11 @@ function setHeader() {
   }
 
   $scope.songInfo = function(){
-
-
-
+    console.log("this is a song")
   }
 
+  $scope.$on("$ionicView.beforeEnter", function(){
+    console.log("do it")
+    $scope.playlist();
+  })
 }]);
