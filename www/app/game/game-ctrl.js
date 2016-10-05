5dd0fbd0cf5f5bf05b7fc4f3cb7e8f7a5989030c
Alec <email": null>
2016-10-04 20:58:15 -0700

    Add rollit feature

--- www/app/game/game-ctrl.js
+++ www/app/game/game-ctrl.js
@@ -1,11 +1,3 @@
-// $.ajax({
-//   url: rootUrl,
-//   method: 'GET'
-// })
-// .done(function(response) {
-//   userPlaylistSongs = response
-// });
-
 function playlistShuffle(){
   index = Math.floor(Math.random() * userPlaylistSongs.length + 1)
   return userPlaylistSongs[index]
@@ -13,6 +5,50 @@ function playlistShuffle(){
 
 karaoke.controller('GameCtrl', ['$http', '$scope', function($http, $scope){
 
+    $scope.myTrigger = false;
+    $scope.btnClick = function () {
+      $scope.myTrigger = !$scope.myTrigger;
+    };
+    console.log('inscope');
+}]).directive('slot', ['$animate', function ($animate) {
+    var der = {
+        restrict: 'E',
+        template: '<div class="slots" id="slots"><div class="wrapper"></div></div>',
+        fn: {
+            opts: ['Abraham','Bob','Carter','Doug','Fred'],
+            go: function (elem) {
+                der.fn.addSlots(elem);
+                der.fn.moveSlots(elem);
+            },
+            addSlots: function (elem) {
+                for(var i = 0; i < 15; i++){
+                    var ctr = Math.floor(Math.random()*der.fn.opts.length);
+                    elem.append("<div class='slot'>"+der.fn.opts[ctr]+"</div>");
+                }
+            },
+            moveSlots: function (elem) {
+                var marginTop = parseInt(elem.css("margin-top"), 10);
+                marginTop -= (7 * 100);
 
+                var time = 600;
+                time += Math.round(Math.random()*1000);
+                elem.stop(true,true);
 
+                elem.animate({"margin-top": marginTop+"px"},{'duration' : time});
+            }
+        },
+        link: function (scope, ele, attrs) {
+            if (attrs.trigger && attrs.trigger !== undefined) {
+                if (scope[attrs.trigger] !== undefined) {
+                    scope.$watch(attrs.trigger, function (newValue, oldValue) {
+                        if (newValue) {
+                            der.fn.go($('#slots .wrapper'));
+                        }
+                    });
+                }
+            }
+        }
+    };
+    console.log(der)
+    return der;
 }]);
