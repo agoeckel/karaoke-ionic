57dd572b81ecee5960801173e788e4bb4231019d
Alec <email": null>
2016-10-04 23:59:25 -0700

    Add hide and show wheel

--- www/app/game/game-ctrl.js
+++ www/app/game/game-ctrl.js
@@ -5,50 +5,9 @@ function playlistShuffle(){
 
 karaoke.controller('GameCtrl', ['$http', '$scope', function($http, $scope){
 
-    $scope.myTrigger = false;
-    $scope.btnClick = function () {
-      $scope.myTrigger = !$scope.myTrigger;
-    };
-    console.log('inscope');
-}]).directive('slot', ['$animate', function ($animate) {
-    var der = {
-        restrict: 'E',
-        template: '<div class="slots" id="slots"><div class="wrapper"></div></div>',
-        fn: {
-            opts: ['Abraham','Bob','Carter','Doug','Fred'],
-            go: function (elem) {
-                der.fn.addSlots(elem);
-                der.fn.moveSlots(elem);
-            },
-            addSlots: function (elem) {
-                for(var i = 0; i < 15; i++){
-                    var ctr = Math.floor(Math.random()*der.fn.opts.length);
-                    elem.append("<div class='slot'>"+der.fn.opts[ctr]+"</div>");
-                }
-            },
-            moveSlots: function (elem) {
-                var marginTop = parseInt(elem.css("margin-top"), 10);
-                marginTop -= (7 * 100);
+  $scope.IsHidden = true;
+  $scope.btnClick = function(){
+      $scope.IsHidden = $scope.IsHidden ? false : true;
+    }
 
-                var time = 600;
-                time += Math.round(Math.random()*1000);
-                elem.stop(true,true);
-
-                elem.animate({"margin-top": marginTop+"px"},{'duration' : time});
-            }
-        },
-        link: function (scope, ele, attrs) {
-            if (attrs.trigger && attrs.trigger !== undefined) {
-                if (scope[attrs.trigger] !== undefined) {
-                    scope.$watch(attrs.trigger, function (newValue, oldValue) {
-                        if (newValue) {
-                            der.fn.go($('#slots .wrapper'));
-                        }
-                    });
-                }
-            }
-        }
-    };
-    console.log(der)
-    return der;
 }]);
